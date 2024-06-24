65caf36a1f9b38df0708d6836a4198db078b8071
Minh Khang <trmkhng2001@gmail.com>
2024-06-24 09:18:10 +0700

    update

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 const styles = [
   { class: "sb_payments", content: "Thanh Toán Nè" },
-  { class: "another_class", content: "Nội Dung Khác" },
+  { class: "sb_opportunities", content: "Quy Trình Bán Hàng" },
+  { class: "sb_contacts", content: "Liên Hệ" },
+  { class: "sb_conversations", content: "Hội Thoại" },
+  { class: "sb_settings", content: "Cài Đặt" },
+
   // Thêm các đối tượng khác nếu cần
 ];
 
 function applyStyles() {
   styles.forEach((style) => {
     const css = `
-          .${style.class} span {
+          #${style.class} span {
             text-indent: -999999999px;
             line-height: 0;
           }
 
-          .${style.class} span::after {
+          #${style.class} span::after {
             content: "${style.content}";
             line-height: initial;
             display: block;
